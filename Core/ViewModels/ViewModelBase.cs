46141fb775957b83b8cbb90e1a197da4d9af345e
Jakub Barwa <69699459+B4rawasz@users.noreply.github.com>
2026-06-20 19:56:10 +0200

    App init

---
+++ Core/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace StreamBurst.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ Core/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace StreamBurst.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
