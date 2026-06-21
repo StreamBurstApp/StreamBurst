25520a58689d82095d3eb824888b4dd649d0c9f4
Jakub Barwa <69699459+B4rawasz@users.noreply.github.com>
2026-06-21 10:40:43 +0200

    Add event bus

---
+++ Abstractions/EventBus/IEventBus.cs
@@ -0,0 +1,15 @@
+﻿using Abstractions.Event;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstractions.EventBus
+{
+    public interface IEventBus
+    {
+        void Publish(ModuleEvent moduleEvent);
+        IDisposable Subscribe(string? sourceModuleId, string? eventTypem, Action<ModuleEvent> handler);
+        IDisposable SubscribeAsync(string? sourceModuleId, string? eventType, Func<ModuleEvent, Task> handler);
+        ModuleEvent? GetLastValue(string sourceModuleId, string eventType);
+    }
+}
