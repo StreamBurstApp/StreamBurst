59922f6ab5d6870dcb1a924c481f6220b3f34b78
Jakub Barwa <69699459+B4rawasz@users.noreply.github.com>
2026-06-21 10:35:51 +0200

    Add actions

---
+++ Abstractions/Event/ActionDescriptor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstractions.Event
+{
+    public sealed record ActionDescriptor
+    {
+        public required string ActionId { get; init; }
+        public required string DisplayName { get; init; }
+        public required ParamDescriptor[] Parameters { get; init; }
+    }
+}
