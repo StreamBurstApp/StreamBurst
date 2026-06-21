11c7009044945428fa7877e7cab5576c2dab3dcb
Jakub Barwa <69699459+B4rawasz@users.noreply.github.com>
2026-06-21 10:27:01 +0200

    Add module event interface

---
+++ Abstractions/Event/EventValueKind.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstractions.Event
+{
+    public enum EventValueKind
+    {
+        None,
+        Bool,
+        Number,
+        String,
+        Json,
+        Trigger
+    }
+}
---
+++ Abstractions/Event/ModuleEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstractions.Event
+{
+    public sealed record ModuleEvent
+    {
+        public required string SourceModuleId { get; init; }
+        public required string EventType { get; init; }
+        public required EventValueKind ValueKind { get; init; }
+        public object? Value { get; init; }
+        public DateTimeOffset Timestamp { get; init; } = DateTimeOffset.UtcNow;
+        public IReadOnlyDictionary<string, string>? Tags { get; init; }
+    }
+}
